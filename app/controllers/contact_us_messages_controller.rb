c60b83d032b38442d34c786c141de452162c86fd
Stephen Kendrick <skendri1@my.westga.edu>
2014-03-02 17:54:59 -0500

    #44 improved styling of users (index and show), POIs index, rules, contact us, admin.

--- app/controllers/contact_us_messages_controller.rb
+++ app/controllers/contact_us_messages_controller.rb
@@ -9,7 +9,8 @@ class ContactUsMessagesController < ApplicationController
   # Handels creating the message to send to our email
   def create
     ContactUsMailer.send_message(params[:contact_us_message][:name].to_s, params[:contact_us_message][:email].to_s, params[:contact_us_message][:subject].to_s, params[:contact_us_message][:message].to_s).deliver
-    redirect_to '/contact_us_messages/confirmation'
+    flash[:notice] = "Message was sent. Thank you, we will try to reply in a timely manner."
+    redirect_to '/contact_us_messages/new'
   end
   
   # GET /contact_us_messages/confirmation
