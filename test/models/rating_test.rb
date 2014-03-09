07abdb22a0bfc4e91ef79e9b04866c70733f4f2e
Justin Walker <jwalke21@my.westga.edu>
2014-03-08 20:37:18 -0500

    #78 Set up ratings fixture and tested to ensure those fixtures set properly.

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class RatingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "user is set" do
+		assert_equal users(:stephen), ratings(:secondrating).user, "user did not match expected"
+	end
+
+	test "poi is set" do
+		assert_equal point_of_interests(:firstpoi), ratings(:firstrating).point_of_interest, "POI did not match expected"
+	end
+
+	test "first rating is set" do
+		assert_equal 5, ratings(:firstrating).rating, "First rating did not match expected"
+	end
+
+	test "second rating is set" do
+		assert_equal 1, ratings(:secondrating).rating, "Second rating did not match expected"
+	end
 end
