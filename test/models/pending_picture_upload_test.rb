8533bce9e93730811552f49b1095eb34b470b4fb
Justin Walker <jwalke21@my.westga.edu>
2014-03-08 21:06:32 -0500

    #78 Pending picture uploads fixture populated and tested.

--- test/models/pending_picture_upload_test.rb
+++ test/models/pending_picture_upload_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PendingPictureUploadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "user is set" do
+		assert_equal users(:justin), pending_picture_uploads(:firstpending).user, "user did not match expected"
+	end
+
+	test "picture is set" do
+		assert_equal pictures(:firstpicture), pending_picture_uploads(:firstpending).picture, "picture did not match expected"
+	end
 end
