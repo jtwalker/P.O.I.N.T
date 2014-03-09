947f49a4a044c74a41f980d4361ccf173cb266f6
Justin Walker <jwalke21@my.westga.edu>
2014-03-08 20:19:49 -0500

    #78 Added tests to point_of_interest that would make sure the fixtures were set properly.
    Set up and tested comments.

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "user id is set" do
+		assert_equal 500740463, comments(:firstcomment).user_id, "user id did not match expected"
+	end
+
+	test "poi id is set" do
+		assert_equal 867678396, comments(:firstcomment).point_of_interest_id, "Uid did not match expected"
+	end
+
+	test "comment message is set" do
+		assert_equal "MyTextToo", comments(:secondcomment).body, "comment message did not match expected"
+	end
 end
--- test/models/point_of_interest_test.rb
+++ test/models/point_of_interest_test.rb
@@ -1,6 +1,30 @@
 require 'test_helper'
 
 class PointOfInterestTest < ActiveSupport::TestCase
+	test "latitude is set" do
+		assert_equal 1.5, point_of_interests(:firstpoi).latitude, "latitude did not match expected"
+	end
+
+	test "longitude is set" do
+		assert_equal 1.5, point_of_interests(:firstpoi).longitude, "longitude did not match expected"
+	end
+
+	test "summary is set" do
+		assert_equal "MyText", point_of_interests(:firstpoi).summary, "summary did not match expected"
+	end
+
+	test "artist info is set" do
+		assert_equal "MyText", point_of_interests(:firstpoi).artist_info, "artist info did not match expected"
+	end
+
+	test "sponsor info is set" do
+		assert_equal "MyText", point_of_interests(:firstpoi).sponsor_info, "sponsor info did not match expected"
+	end
+
+	test "user id is set" do
+		assert_equal 500740463, point_of_interests(:firstpoi).user_id, "user id did not match expected"
+	end
+
 	test "should not create poi without latitude" do
 		poi = PointOfInterest.new
 		poi.longitude = 1
