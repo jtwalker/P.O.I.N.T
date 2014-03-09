022e560d608615a8d098290154eb69216aba88d8
Justin Walker <jwalke21@my.westga.edu>
2014-03-08 20:57:59 -0500

    #78 Set up picture fixture and tested the fixture. (This one would not work the same as the previous ones; I have two assumptions... I'll comment in the issue)

--- test/models/picture_test.rb
+++ test/models/picture_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PictureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "user is set" do
+		assert_equal 1, pictures(:firstpicture).user_id, "user did not match expected"
+	end
+
+	test "poi is set" do
+		assert_equal 1, pictures(:firstpicture).point_of_interest_id, "poi did not match expected"
+	end
+
+	test "main image true is set" do
+		assert_equal true, pictures(:firstpicture).main_image, "main image did not match expected"
+	end
+
+	test "main image false is set" do
+		assert_equal false, pictures(:secondpicture).main_image, "main image did not match expected"
+	end
 end
