2c537456e63371f6a07fe62ce02452e8a47b41b6
Stephen Kendrick <skendri1@my.westga.edu>
2014-03-09 16:58:28 -0400

    Fixed contact us so no one can send us an empty message

--- app/controllers/contact_us_messages_controller.rb
+++ app/controllers/contact_us_messages_controller.rb
@@ -8,9 +8,17 @@ class ContactUsMessagesController < ApplicationController
   
   # Handels creating the message to send to our email
   def create
-    ContactUsMailer.send_message(params[:contact_us_message][:name].to_s, params[:contact_us_message][:email].to_s, params[:contact_us_message][:subject].to_s, params[:contact_us_message][:message].to_s).deliver
-    flash[:notice] = "Message was sent. Thank you, we will try to reply in a timely manner."
+
+    if (params[:contact_us_message][:name].blank? || params[:contact_us_message][:email].blank? || params[:contact_us_message][:subject].blank? || params[:contact_us_message][:message].blank?)
+      flash[:alert] = "Please fill out all form areas"
+    else
+
+      ContactUsMailer.send_message(params[:contact_us_message][:name].to_s, params[:contact_us_message][:email].to_s, params[:contact_us_message][:subject].to_s, params[:contact_us_message][:message].to_s).deliver
+      flash[:notice] = "Message was sent. Thank you, we will try to reply in a timely manner."
+    end
+
     redirect_to '/contact_us_messages/new'
+
   end
   
 end
